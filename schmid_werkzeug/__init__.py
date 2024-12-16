fafc8cb6020006e7adaedf29d6463693b5917112
binbash <danielbinschmid@outlook.de>
2024-12-16 11:10:03 +0100

    print_info, print_warning, print_error

--- schmid_werkzeug/__init__.py
+++ schmid_werkzeug/__init__.py
@@ -1,1 +1,2 @@
 from .ml_workflow import get_sample_batches
+from .general_utils import print_error, print_info, print_warning
---
+++ schmid_werkzeug/general_utils.py
@@ -0,0 +1,9 @@
+def print_info(*args):
+    print("[INFO]", *args)
+
+def print_warning(*args):
+    print("[WARNING]", *args)
+
+
+def print_error(*args):
+    print("[ERROR]", *args)
